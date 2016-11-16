8c965ba009f8c9cd49912c66fc3f9c9457c024e7
Sandeep Gunduboyina <sandeepgy11@gmail.com>
2016-11-16 19:35:00 +0530

    Integrate sass file loading for dev environment
        - Adds style tag to the document
        - Hot reloading will work as it is

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -3,6 +3,12 @@ var webpack = require('webpack');
 var config = require('./webpack.base');
 var rootPath = path.resolve(__dirname, '../');
 
+config.module.loaders.push({
+  test: /\.scss$/,
+  loaders: ['style', 'css', 'sass'],
+  include: path.join(rootPath, 'src')
+});
+
 config.entry = [
   'webpack-dev-server/client?http://0.0.0.0:3000',
   'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
