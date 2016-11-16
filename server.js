3cecd3b6452fad133f0904fc7e0f3b0bedb53b00
Sandeep Gunduboyina <sandeepgy11@gmail.com>
2016-11-16 16:10:50 +0530

    Add node server to serve and hot reload bundle

---
+++ server.js
@@ -0,0 +1,20 @@
+var path  = require('path');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./scripts/webpack.dev');
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    quiet: false,
+    stats: {
+        colors: true
+    },
+    historyApiFallback: true
+}).listen(3000, "0.0.0.0", function (err) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log("Listening at localhost:3000");
+});
