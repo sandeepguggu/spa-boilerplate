93f53b00a35b278c582af36c33e3a88f860c3c4e
Sandeep Gunduboyina <sandeepgy11@gmail.com>
2016-11-16 16:17:29 +0530

    Change indentation from 4 spaces to 2 spaces

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -4,20 +4,20 @@ var config = require('./webpack.base');
 var rootPath = path.resolve(__dirname, '../');
 
 config.entry = [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-    './src/index'
+  'webpack-dev-server/client?http://0.0.0.0:3000',
+  'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+  './src/index'
 ];
 
 config.output = {
-    path: rootPath,
-    filename: "bundle.js",
-    publicPath: "/static/"
+  path: rootPath,
+  filename: "bundle.js",
+  publicPath: "/static/"
 };
 
 config.plugins = [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin()
 ];
 
 config.devtool = '[inline-]source-map';
--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./scripts/webpack.dev');
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    quiet: false,
-    stats: {
-        colors: true
-    },
-    historyApiFallback: true
+  publicPath: config.output.publicPath,
+  hot: true,
+  quiet: false,
+  stats: {
+    colors: true
+  },
+  historyApiFallback: true
 }).listen(3000, "0.0.0.0", function (err) {
-    if (err) {
-        console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 
-    console.log("Listening at localhost:3000");
+  console.log("Listening at localhost:3000");
 });
