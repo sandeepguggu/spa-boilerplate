6ca425e35cd8b1b0af0cc445904053c4496d1d69
Sandeep Gunduboyina <sandeepgy11@gmail.com>
2016-11-16 16:10:20 +0530

    Add react hot loader and entry file to webpack.dev.js

---
+++ scripts/webpack.dev.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.base');
+var rootPath = path.resolve(__dirname, '../');
+
+config.entry = [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index'
+];
+
+config.output = {
+    path: rootPath,
+    filename: "bundle.js",
+    publicPath: "/static/"
+};
+
+config.plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+];
+
+config.devtool = '[inline-]source-map';
+
+module.exports = config;
