cfd95c39ab1a7ac0ec71082229265ada56932ff5
Sandeep Gunduboyina <sandeepgy11@gmail.com>
2016-11-28 22:45:37 +0530

    Remove image from index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import 'utils/api';
 
-const bulbSVG = require('images/bulb.svg?raw=true');
 import './base.scss';
 render(
-  <span dangerouslySetInnerHTML={{__html: bulbSVG}} />,
+  <span>SPA Boilerplate</span>,
   document.querySelector('#root')
 );
